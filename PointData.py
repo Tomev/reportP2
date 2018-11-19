df181ad2d4350c70c919195ce26c6c72260153ca
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-11-19 12:59:09 +0100

    Added point class.

---
+++ PointData.py
@@ -0,0 +1,19 @@
+from enum import IntEnum
+
+class LineParts(IntEnum):
+    TIME    = 0
+    CHAN_A  = 1
+    CHAN_B  = 2
+
+
+class P2Point:
+
+    time = 0
+    chan = 0
+
+    def __init__(self, line, channel):
+        line_parts = line.split('\t')
+        self.time = line_parts[LineParts.TIME]
+        self.chan = line_parts[channel]
+
+
